45910623210aca782e11db5c2830563d4ba1da3b
Tyler Dakin <dakint@icloud.com>
2019-12-28 00:12:17 -0500

    Add first multiple game functionality

--- main.py
+++ main.py
@@ -2,244 +2,227 @@ from random import *
 import sys
 from isInt import isInt
 
-types = ["C","D","H","S"]
-numbers = ["2","3","4","5","6","7","8","9","10","11","12","13","14"]
+def playGame(players):
+    types = ["C","D","H","S"]
+    numbers = ["2","3","4","5","6","7","8","9","10","11","12","13","14"]
 
-cards = []
+    cards = []
 
-for type in types:
-    for number in numbers:
-        cards.append(type + "-" + number)
-print("Welcome to WAR")
+    for type in types:
+        for number in numbers:
+            cards.append(type + "-" + number)
 
-def getPlayerCount():
-    print("Number of players: ", end="")
-    playerCount = input()
-    if (not (isInt(playerCount)) or int(playerCount) < 2):
-        if (not isInt(playerCount)):
-            print("Please enter a valid integer of at least 2.")
-            return getPlayerCount()
-        elif int(playerCount) == 1:
-            print("Ah, a lonely game, I see.")
-            print("You win by default!")
-            print("Congratulations!")
-        elif int(playerCount) == 0:
-            print("No one showed up so I guess I win!")
-        elif int(playerCount) < 0:
-            print("Hmmm, negative players? Odd...")
 
-    else:
-        return int(playerCount)
 
-playerCount = getPlayerCount()
 
-if (playerCount == None):
-    sys.exit()
+    playerCount = players
 
-print("Great! " + str(playerCount) + " players! Let's get the deck shuffled and dealt.")
 
-numCardsEach = int(len(cards)/playerCount)
-print("Dealing " + str(numCardsEach) + " cards to each player!")
 
-def dealCards():
-    dict = {}
-    shuffle(cards)
+    print("Great! " + str(playerCount) + " players! Let's get the deck shuffled and dealt.")
 
-    #create the players and store them
-    for i in range(playerCount):
-        playerCards = [[],[]]
-        player = "Player " + str(i + 1)
-        dict[player] = playerCards
+    numCardsEach = int(len(cards)/playerCount)
+    print("Dealing " + str(numCardsEach) + " cards to each player!")
 
-    lastPlayer = "Player " + str(playerCount)
+    def dealCards():
+        dict = {}
+        shuffle(cards)
 
-    #give players cards
-    for card in cards:
-        lastPlayerNumber = int(lastPlayer[len(lastPlayer)-1]) #gets last letter
-        if lastPlayerNumber == playerCount:
-            lastPlayer = "Player 1"
+        #create the players and store them
+        for i in range(playerCount):
+            playerCards = [[],[]]
+            player = "Player " + str(i + 1)
+            dict[player] = playerCards
+
+        lastPlayer = "Player " + str(playerCount)
+
+        #give players cards
+        for card in cards:
+            lastPlayerNumber = int(lastPlayer[len(lastPlayer)-1]) #gets last letter
+            if lastPlayerNumber == playerCount:
+                lastPlayer = "Player 1"
+            else:
+                lastPlayer = "Player " + str(lastPlayerNumber + 1)
+
+            currentCards = dict[lastPlayer][0]
+            currentCards.append(card)
+            dict[lastPlayer][0] = currentCards
+
+        return dict
+
+
+    allPlayerCards = dealCards()
+    print("The cards have been dealt!")
+
+    round = 0
+    playersWithCards = list(allPlayerCards.keys())
+
+    def doWar(winner, cardNumbers, cardsIn, playersPassed):
+        # Handle two winners
+        # 2.2.1 Find People in War
+        playersInWar = []
+        playersInWar.extend(playersPassed)
+        warPeople = []
+        # while (winner in cardNumbers):
+        #     index = cardNumbers.index(winner)
+        #
+        #     # Only include a player if they have some card(s)
+        #     if (len(allPlayerCards[(playersInWar)[index]][0]) > 0):
+        #         cardNumbers.remove(cardNumbers[index])
+        #         warPeople.append((playersInWar)[index])
+        #         playersInWar.remove(playersInWar[index])
+        #     else:
+        #         victim = (cardNumbers)[index]
+        #         del cardNumbers[index]
+        #         # cardNumbers.append(victim)
+        #         playersInWar.remove(playersInWar[index])
+
+
+        for i in range(len(cardNumbers)):
+            if cardNumbers[i] == winner:
+                if len(allPlayerCards[playersInWar[i]][0]) > 0:
+                    warPeople.append(playersInWar[i])
+
+        print("warPeople = ", end ="")
+        print(playersInWar)
+        print(warPeople)
+
+        if (len(warPeople) == 1):
+            allPlayerCards[warPeople[0]][1].extend(cardsIn)
+
+            return warPeople[0]
+        elif (len(warPeople) == 0):
+            winner = sample(playersPassed,1)[0]
+            allPlayerCards[winner][1].extend(cardsIn)
+            return winner
+
+        # 2.2.2 If any people have < 4 in the [0], get the lowest len for the [0]s and do War with that number of cards
+        wagingNumber = 4
+        for person in warPeople:
+            if (len(allPlayerCards[person][0]) < wagingNumber):
+                print("Setting wage number...")
+                wagingNumber = len(allPlayerCards[person][0])
+            else:
+                print("Not setting wage number: ")
+                print("cards for person: ")
+                print(allPlayerCards[person][0])
+
+        # 2.2.3 Put the first few cards into the wagingPile
+            # STEP 1: Get one card from each player
+        cardPile = []
+        comparePile = []
+        for i in range(len(warPeople)):
+            playerID = (warPeople)[i]
+            for j in range(wagingNumber):
+                if len(allPlayerCards[playerID][0]) < 1:
+                    print("Sample larger than population!!")
+                    print("Waging: " + str(wagingNumber))
+                    print("j = " + str(j))
+                thisCard = sample(allPlayerCards[playerID][0],1)[0]
+                cardPile.append(thisCard)
+                if j == (wagingNumber - 1): # this is the card to compare with the others
+                    print("printing... " + str(j))
+                    comparePile.append(thisCard)
+                allPlayerCards[playerID][0].remove(thisCard)
+
+
+
+        comparedNumbers = []
+        for card in comparePile:
+            comparedNumbers.append(int(card.split("-")[1]))
+
+        # Get winning number
+        winner = max(comparedNumbers)
+        winningPlayer = ""
+
+        if (cardNumbers.count(winner) > 1):
+            cardPile.extend(cardsIn)
+            return doWar(winner, comparedNumbers, cardPile, warPeople)
+
         else:
-            lastPlayer = "Player " + str(lastPlayerNumber + 1)
+            # Get winning player
+            index = comparedNumbers.index(winner)
+            winningPlayer = (warPeople)[index]
 
-        currentCards = dict[lastPlayer][0]
-        currentCards.append(card)
-        dict[lastPlayer][0] = currentCards
-
-    return dict
-
-
-allPlayerCards = dealCards()
-print("The cards have been dealt!")
-
-round = 0
-playersWithCards = list(allPlayerCards.keys())
-
-def doWar(winner, cardNumbers, cardsIn, playersPassed):
-    # Handle two winners
-    # 2.2.1 Find People in War
-    playersInWar = []
-    playersInWar.extend(playersPassed)
-    warPeople = []
-    # while (winner in cardNumbers):
-    #     index = cardNumbers.index(winner)
-    #
-    #     # Only include a player if they have some card(s)
-    #     if (len(allPlayerCards[(playersInWar)[index]][0]) > 0):
-    #         cardNumbers.remove(cardNumbers[index])
-    #         warPeople.append((playersInWar)[index])
-    #         playersInWar.remove(playersInWar[index])
-    #     else:
-    #         victim = (cardNumbers)[index]
-    #         del cardNumbers[index]
-    #         # cardNumbers.append(victim)
-    #         playersInWar.remove(playersInWar[index])
+            # give cardsIn + cardPile to winner
+            cardPile.extend(cardsIn)
+            allPlayerCards[winningPlayer][1].extend(cardPile)
 
 
-    for i in range(len(cardNumbers)):
-        if cardNumbers[i] == winner:
-            if len(allPlayerCards[playersInWar[i]][0]) > 0:
-                warPeople.append(playersInWar[i])
+            return winningPlayer
 
-    print("warPeople = ", end ="")
-    print(playersInWar)
-    print(warPeople)
+    def playRound(players):
+        print("Starting Round " + str(round) + "!")
+        print("Players in the game: " + str(len(playersWithCards)))
 
-    if (len(warPeople) == 1):
-        allPlayerCards[warPeople[0]][1].extend(cardsIn)
-
-        return warPeople[0]
-    elif (len(warPeople) == 0):
-        winner = sample(playersPassed,1)[0]
-        allPlayerCards[winner][1].extend(cardsIn)
-        return winner
-
-    # 2.2.2 If any people have < 4 in the [0], get the lowest len for the [0]s and do War with that number of cards
-    wagingNumber = 4
-    for person in warPeople:
-        if (len(allPlayerCards[person][0]) < wagingNumber):
-            print("Setting wage number...")
-            wagingNumber = len(allPlayerCards[person][0])
-        else:
-            print("Not setting wage number: ")
-            print("cards for person: ")
-            print(allPlayerCards[person][0])
-
-    # 2.2.3 Put the first few cards into the wagingPile
         # STEP 1: Get one card from each player
-    cardPile = []
-    comparePile = []
-    for i in range(len(warPeople)):
-        playerID = (warPeople)[i]
-        for j in range(wagingNumber):
-            if len(allPlayerCards[playerID][0]) < 1:
-                print("Sample larger than population!!")
-                print("Waging: " + str(wagingNumber))
-                print("j = " + str(j))
+        cardsIn = []
+        for i in range(len(playersWithCards)):
+            playerID = (playersWithCards)[i]
             thisCard = sample(allPlayerCards[playerID][0],1)[0]
-            cardPile.append(thisCard)
-            if j == (wagingNumber - 1): # this is the card to compare with the others
-                print("printing... " + str(j))
-                comparePile.append(thisCard)
+            cardsIn.append(thisCard)
+            print(allPlayerCards[playerID][0])
+            print(thisCard)
             allPlayerCards[playerID][0].remove(thisCard)
 
+        # STEP 2: Compare cards
+        # 2.1 Convert cards to values
+        cardNumbers = []
+        for card in cardsIn:
+            cardNumbers.append(int(card.split("-")[1]))
 
+        # Get winning number
+        winner = max(cardNumbers)
+        winningPlayer = ""
 
-    comparedNumbers = []
-    for card in comparePile:
-        comparedNumbers.append(int(card.split("-")[1]))
+        if (cardNumbers.count(winner) > 1):
+            # doWar will handle the winnings
+            winningPlayer = doWar(winner, cardNumbers, cardsIn, (playersWithCards))
 
-    # Get winning number
-    winner = max(comparedNumbers)
-    winningPlayer = ""
+        else:
+            # Get winning player
+            index = cardNumbers.index(winner)
+            winningPlayer = (playersWithCards)[index]
 
-    if (cardNumbers.count(winner) > 1):
-        cardPile.extend(cardsIn)
-        return doWar(winner, comparedNumbers, cardPile, warPeople)
+            #handle winnings
+            allPlayerCards[winningPlayer][1].extend(cardsIn)
 
-    else:
-        # Get winning player
-        index = comparedNumbers.index(winner)
-        winningPlayer = (warPeople)[index]
+        # STEP 4 & 5: Move [1] to [0] if a player's [0] is empty but the [1] is not
+        #             Eliminate players with no cards
+        playersToRemove = []
 
-        # give cardsIn + cardPile to winner
-        cardPile.extend(cardsIn)
-        allPlayerCards[winningPlayer][1].extend(cardPile)
+        for person in playersWithCards:
+            print(person)
+            print(allPlayerCards[person][0])
+            if len(allPlayerCards[person][0]) == 0:
 
+                print(allPlayerCards[person][1])
+                if (len(allPlayerCards[person][1]) == 0):
+                    (playersToRemove).append(person)
+                    print(person + " has been eliminated!")
+                else:
+                    allPlayerCards[person][0].extend(allPlayerCards[person][1])
+                    shuffle(allPlayerCards[person][0])
+                    allPlayerCards[person][1] = []
 
+        while len(playersToRemove) > 0:
+            print(playersToRemove[0])
+            print(playersWithCards)
+            playersWithCards.remove(playersToRemove[0])
+            del playersToRemove[0]
+
+        #this should be done last
         return winningPlayer
 
-def playRound(players):
-    print("Starting Round " + str(round) + "!")
-    print("Players in the game: " + str(len(playersWithCards)))
 
-    # STEP 1: Get one card from each player
-    cardsIn = []
-    for i in range(len(playersWithCards)):
-        playerID = (playersWithCards)[i]
-        thisCard = sample(allPlayerCards[playerID][0],1)[0]
-        cardsIn.append(thisCard)
-        print(allPlayerCards[playerID][0])
-        print(thisCard)
-        allPlayerCards[playerID][0].remove(thisCard)
 
-    # STEP 2: Compare cards
-    # 2.1 Convert cards to values
-    cardNumbers = []
-    for card in cardsIn:
-        cardNumbers.append(int(card.split("-")[1]))
 
-    # Get winning number
-    winner = max(cardNumbers)
-    winningPlayer = ""
 
-    if (cardNumbers.count(winner) > 1):
-        # doWar will handle the winnings
-        winningPlayer = doWar(winner, cardNumbers, cardsIn, (playersWithCards))
 
-    else:
-        # Get winning player
-        index = cardNumbers.index(winner)
-        winningPlayer = (playersWithCards)[index]
-
-        #handle winnings
-        allPlayerCards[winningPlayer][1].extend(cardsIn)
-
-    # STEP 4 & 5: Move [1] to [0] if a player's [0] is empty but the [1] is not
-    #             Eliminate players with no cards
-    playersToRemove = []
-
-    for person in playersWithCards:
-        print(person)
-        print(allPlayerCards[person][0])
-        if len(allPlayerCards[person][0]) == 0:
-
-            print(allPlayerCards[person][1])
-            if (len(allPlayerCards[person][1]) == 0):
-                (playersToRemove).append(person)
-                print(person + " has been eliminated!")
-            else:
-                allPlayerCards[person][0].extend(allPlayerCards[person][1])
-                shuffle(allPlayerCards[person][0])
-                allPlayerCards[person][1] = []
-
-    while len(playersToRemove) > 0:
-        print(playersToRemove[0])
+    while (len(playersWithCards) > 1):
+        round += 1
+        roundWinner = playRound((playersWithCards))
+        print(roundWinner + " wins Round " + str(round) + "!\n\n\n")
         print(playersWithCards)
-        playersWithCards.remove(playersToRemove[0])
-        del playersToRemove[0]
 
-    #this should be done last
-    return winningPlayer
-
-
-
-
-
-
-while (len(playersWithCards) > 1):
-    round += 1
-    roundWinner = playRound((playersWithCards))
-    print(roundWinner + " wins Round " + str(round) + "!\n\n\n")
-    print(playersWithCards)
-
-print(playersWithCards[0] + " has WON the Game of WAR!!!")
+    return playersWithCards[0]
--- manyGames.py
+++ manyGames.py
@@ -1,4 +1,56 @@
 from main import *
 
-for game in range(5):
-    #add some stuff here when ready
+def getPlayerCount():
+    print("Number of players: ", end="")
+    playerCount = input()
+    if (not (isInt(playerCount)) or int(playerCount) < 2):
+        if (not isInt(playerCount)):
+            print("Please enter a valid integer of at least 2.")
+            return getPlayerCount()
+        elif int(playerCount) == 1:
+            print("Ah, a lonely game, I see.")
+            print("You win by default!")
+            print("Congratulations!")
+        elif int(playerCount) == 0:
+            print("No one showed up so I guess I win!")
+        elif int(playerCount) < 0:
+            print("Hmmm, negative players? Odd...")
+
+    else:
+        return int(playerCount)
+
+def getNumOfGames():
+    print("Number of games: ", end="")
+    gameCount = input()
+    if (not (isInt(gameCount)) or int(gameCount) < 1):
+        if (not isInt(gameCount)):
+            print("Please enter a valid integer of at least 1.")
+            return getNumOfGames()
+
+        elif int(gameCount) == 0:
+            print("No game, no shame! But now nothing happens...")
+            sys.exit()
+        elif int(gameCount) < 0:
+            print("I really don't know how to do that...\nSorry.")
+
+    else:
+        return int(gameCount)
+
+print("Welcome to WAR")
+games = getNumOfGames()
+players = getPlayerCount()
+
+if (players == None) or (games == None):
+    sys.exit()
+
+count = 0
+winCount = {}
+while (count < games):
+    count += 1
+    winner = playGame(players)
+    if winner in list(winCount.keys()):
+        winCount[winner] = winCount[winner] + 1
+    else:
+        winCount[winner] = 1
+
+print(winCount)
